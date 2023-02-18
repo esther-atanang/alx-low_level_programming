a9697bbdc7eaf091c931a91be65688ec72aff70d
po-w-er <atanang.esthere@gmail.com>
2023-02-18 13:18:57 +0000

    correcting file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main(){
-    char Mytext[] = "Programming is like building a multilingual puzzle";
+int main(void)
+{
+
+    char Mytext[] = "Programming is like building a multilingual puzzle \n";
+
     puts(Mytext);
-    return 0;
+    return(0);
 }
