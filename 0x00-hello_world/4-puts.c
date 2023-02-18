684ccc164ce468d2804c6d6ad44e9b81b20a44e8
po-w-er <atanang.esthere@gmail.com>
2023-02-18 13:36:51 +0000

    changed file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main-prints Programmin g is like building a multilingual puzzle
+ */
 int main(void)
 {
-
-char Mytext[] = "\"Programming is like building a multilingual puzzle";
-puts(Mytext);
-return(0);
-
+    char Mytext[] = "\"Programming is like building a multilingual puzzle";
+    puts(Mytext);
+    return(0);
 }
