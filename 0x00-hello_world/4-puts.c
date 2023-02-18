8109e4e44177a1d4d3e5d33b4015a25a2d5e1f7f
po-w-er <atanang.esthere@gmail.com>
2023-02-18 13:56:14 +0000

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main: prints Programmin g is like building a multilingual puzzle
+ *main - prints Programming is like building a multilingual puzzle
  *Return: 0 (success)
  */
 int main(void)
