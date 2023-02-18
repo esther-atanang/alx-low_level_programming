3c8236772773fe1d43e9f48c468b819a88bea5e9
po-w-er <atanang.esthere@gmail.com>
2023-02-18 13:24:41 +0000

    another

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 
-    char Mytext[] = "Programming is like building a multilingual puzzle\n";
+    char Mytext[] = "\"Programming is like building a multilingual puzzle";
 
     puts(Mytext);
     return(0);
