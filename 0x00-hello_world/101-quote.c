6c5fba61f5778f6eec1345ddfafb51f17d86b268
po-w-er <atanang.esthere@gmail.com>
2023-02-19 12:21:01 +0000

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+/**
+*main- prints out a quote followed by a new line 
+*Return - 1(Error)
+*/
+int main(void){
+write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+return (1);   
+}
