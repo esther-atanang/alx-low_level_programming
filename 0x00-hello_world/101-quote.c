2cc26d01f814c45953473086200908f6d22aa056
po-w-er <atanang.esthere@gmail.com>
2023-02-19 12:37:44 +0000

    changed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main- prints out a quote followed by a new line 
+*main-prints out a quote followed by a new line 
 *Return:1(Error)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 58);
+write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 return (1);     
 }
