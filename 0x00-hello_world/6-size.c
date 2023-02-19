8c61ff1f17a499a928b783bbbdaec65dc52354dd
po-w-er <atanang.esthere@gmail.com>
2023-02-19 09:23:48 +0000

    another

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,12 @@ int main(void)
 int intType;
 float floatType;
 char charType;
-double doubleType;
-printf("This is the size of an integer: %d", sizeof(intType));
-printf("This is the size of a float: %d", sizeof(floatType));
-printf("This is the size of a character: %d", sizeof(charType));
-printf("This is the size of a double: %d", sizeof(doubleType));
+long int longType;
+long long int longLongType;
+printf("size of an int:%d byte(s)\n", sizeof(intType));
+printf("size of a float:%d byte(s)\n", sizeof(floatType));
+printf("size of a char:%d byte(s)\n", sizeof(charType));
+printf("size of a long int:%d byte(s)\n", sizeof(longType));
+printf("size of a long long int:%d byte(s)\n", sizeof(longLongType));
 return (0);
 }
