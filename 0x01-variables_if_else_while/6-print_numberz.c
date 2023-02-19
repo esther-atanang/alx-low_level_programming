a01d46faac0241ec045f9befeff6ee628ecadfe9
po-w-er <atanang.esthere@gmail.com>
2023-02-19 20:56:07 +0000

    not using char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main - prints from 0 to 9 
-*Return: 0 (success)
+* main - prints from 0 to 9 
+* Return: 0 (success)
 */
 int main(void)
 {
 int i;
-for (i = 0;i <= 9;i++) 
+for (i = 0; i <= 9; i++) 
 {
 putchar('0' + i);
 if (i == 9)
