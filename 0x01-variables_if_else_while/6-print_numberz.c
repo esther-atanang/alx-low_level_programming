13f3ba5465992ed1d86f5c02edb66e26a447c144
po-w-er <atanang.esthere@gmail.com>
2023-02-20 12:52:46 +0000

    new

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@ int main(void)
 {
 int i;
 for (i = 0; i <= 9; i++){ 
-    putchar('0' + i);
-    if (i == 9)
-    {
-        putchar('\n');
-    }
-    }
+putchar('0' + i);
+if (i == 9)
+{
+putchar('\n');
+}
+}
 return (0);
 }
