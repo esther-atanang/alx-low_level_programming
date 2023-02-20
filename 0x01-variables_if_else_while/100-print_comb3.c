c0e49365e1a0a29f4450c3d11dc4f58a798b05ce
po-w-er <atanang.esthere@gmail.com>
2023-02-20 14:50:24 +0000

    new file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet backwards
+* Return: 0 (success)
+*/
+int main()
+{
+for (int i = '0'; i <= '8'; i++)
+{
+for (int j = '1'; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i != '8' || j != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
