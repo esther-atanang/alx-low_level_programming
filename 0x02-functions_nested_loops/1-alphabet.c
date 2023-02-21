15832d26a382eb076a3314fb72cbfd20c6b57a37
po-w-er <atanang.esthere@gmail.com>
2023-02-21 23:59:26 +0000

    alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * main - prints out _putchar
+ * print_alphabet - prints out the alphabets
  * Return: 0 (success)
 */
 void print_alphabet(void)
 {
   char c[] = "abcdefghijklmnopqrstuvwxyz";
   int i;
-
   for (i = 0; i < 26; i++)
   {
-	_putchar(c[i]);
+   _putchar(c[i]);
   }
   _putchar('\n');
 }
