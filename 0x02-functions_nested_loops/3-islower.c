bada75171f8cf1091c2ddcb65bea5783c310fbb6
po-w-er <atanang.esthere@gmail.com>
2023-02-22 08:32:39 +0000

    new commits

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
 /**
- * _islower: checks for if a char is in lowercase
+ * _islower: checks if a char islowercase
  * Return: 0 for (lower) and 1 (upper)
+ * c - an integer
 */
 int _islower(int c){
-    
-    int a_as_int = (int) c;
-    if(a_as_int <= 122 && a_as_int >= 97 ){
-        return (1);
-    }else{
-        return (0);
-    }
-
+int a_as_int = (int) c;
+if(a_as_int <= 122 && a_as_int >= 97 ){
+return (1);
+}else{
+return (0);
+}
 }
