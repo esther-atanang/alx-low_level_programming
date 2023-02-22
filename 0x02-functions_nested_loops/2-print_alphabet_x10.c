73a9a21e422bfa7eab9b8cd03db6f87cde4bbbcb
po-w-er <atanang.esthere@gmail.com>
2023-02-22 00:15:33 +0000

    alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints out the alphabets 10x
+ * Return: 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+char c[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+int number = 10;
+while(number > 0){
+for (i = 0; i < 26; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+number = number - 1;
+}
+
+}
