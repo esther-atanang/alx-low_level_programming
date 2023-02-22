d9bbb7c51b2223b4e8bb3ac191f35402ff04c5d2
po-w-er <atanang.esthere@gmail.com>
2023-02-22 22:03:58 +0000

    new commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table 
+ * Return: the sum of two numbers.
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+_putchar('\n');
+for (j = 0; j < 10; j++)
+{
+int p = i * j;
+if (p == 81)
+{
+_putchar('8');
+_putchar((p % 10) + '0');
+}
+if (p < 10)
+{
+_putchar(p + '0');
+_putchar(',');
+}
+else if (p >= 10 && p != 81)
+{
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+_putchar(',');
+}
+}
+}
+}
