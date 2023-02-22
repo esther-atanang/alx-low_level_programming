1707cf977a25d02a5c959f0bd391ec1539833699
po-w-er <atanang.esthere@gmail.com>
2023-02-22 00:17:51 +0000

    alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,8 @@ void print_alphabet_x10(void)
 char c[] = "abcdefghijklmnopqrstuvwxyz";
 int i;
 int number = 10;
-while(number > 0){
+while (number > 0)
+{
 for (i = 0; i < 26; i++)
 {
 _putchar(c[i]);
