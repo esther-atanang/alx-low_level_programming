0edca3ff844f8867bbe5b3308159dd78fb13837b
po-w-er <atanang.esthere@gmail.com>
2023-02-22 17:35:20 +0000

    another commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,15 @@
  */
 int print_last_digit(int n)
 {
-	int p = n % 10;
-	if( p < 0){
-		p = p * -1;
-		_putchar(p + '0');
-	}else{
-		_putchar(p + '0');
-	}
-	return(p);
+int p = n % 10;
+if (p < 0) 
+{
+p = p * -1;
+_putchar(p + '0');
+}
+else
+{
+_putchar(p + '0');
+}
+return(p);
 }
