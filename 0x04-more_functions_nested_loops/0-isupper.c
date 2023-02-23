1ccd01d506d9bf68a5ac84cb4232fee16e52d9d4
po-w-er <atanang.esthere@gmail.com>
2023-02-23 10:03:18 +0000

    test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,15 @@
  * character in the alphabet
  * Return: (1) for upper and (0) for lower
 */
-int _isupper(char c)
+int _isupper(int c)
 {
 int c_as_int = (int) c;
-if(c_as_int <= 90 && c_as_int >= 65)
+if ((c_as_int <= 90) && (c_as_int >= 65))
 {
 return (1);
-}else{
-return(0);
+}
+else
+{
+return (0);
 }
 }
