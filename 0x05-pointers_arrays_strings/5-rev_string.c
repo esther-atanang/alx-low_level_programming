cc5c5983d01cd8392c2d96e5d5dad8487a5f9e1f
po-w-er <atanang.esthere@gmail.com>
2023-02-27 22:05:55 +0100

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,25 @@
-#include <unistd.h>
 /**
- * rev_string - reverses a string that is passed
- * @s: a string
- * Return - nothing
-*/
-void rev_string(char *s){
-int i = 0;
-int j;
-int d;
-char k[1];
-do {
-if (*s != '\0')
+ * rev_string - reverses a string.
+ * @s: input string to reverse.
+ */
+void rev_string(char *s)
 {
-i++;
-}
-d = i;
-} while (*(s + i) != '\0');
+	int i, j;
+	char r[1000];
 
-for (j = (d - 1); j >= 0; j--)
-{
-k[0] = *(s + j);
-write(1, k, 1);
-}
-write(1,"\n", 1);
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
 }
