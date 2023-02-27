7d90d1b120946bd0ad9888b7ab09b1c82c069d53
po-w-er <atanang.esthere@gmail.com>
2023-02-27 22:43:52 +0100

    push 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,15 @@
 */
 void puts2(char *str)
 {
-    int i, j;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-    for( j= 0; j < i ; j = j + 2){
-       _putchar(str[j]); 
-    }
-    _putchar('\n');
+int i, j;
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+for ( j= 0; j < i ; j = j + 2) 
+{
+_putchar(str[j]); 
+}
+_putchar('\n');
 }
