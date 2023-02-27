ff4d181ca33b0f1a16a7f31707f83b96016fa8ea
po-w-er <atanang.esthere@gmail.com>
2023-02-27 17:59:29 +0100

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - gives the length of a string
+ * @s: the characters
+ * Return: the length of the string.
+*/
+int _strlen(char *s){
+int i = 0;
+int d;
+do {
+i++;
+d = i;
+}
+while(*(s + i) != '\0');
+return d;
+}
