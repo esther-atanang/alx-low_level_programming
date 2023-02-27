60eb0fb3a75c892d3507217a791d651b4d7eb6cb
po-w-er <atanang.esthere@gmail.com>
2023-02-27 19:51:57 +0100

    reverse strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,18 @@
 
 void print_rev(char *s)
 {
+int i = 0;
 int j;
-int k = _strlen(s);
-for(j = (k- 1);j >= 0; j--)
+int d;
+do {
+if (*s != '\0')
+{
+i++;
+}
+d = i;
+} while (*(s + i) != '\0');
+
+for(j = (d- 1);j >= 0; j--)
 {
 _putchar(*(s+j));
 }
