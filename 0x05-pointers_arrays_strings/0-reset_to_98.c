1a829f892dbcade9d440a770dd1eeb744f613c3d
po-w-er <atanang.esthere@gmail.com>
2023-02-27 16:28:49 +0100

    new changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - resets the value to 98
- * @n -pointer to the address of the value 
+ * @n: pointer to the address of the value 
  * Return: returns nothing
  */
 void reset_to_98(int *n)
