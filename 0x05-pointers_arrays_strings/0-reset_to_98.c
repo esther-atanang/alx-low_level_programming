0596b584912e9c31794183f40a6c3dc78cc17d53
po-w-er <atanang.esthere@gmail.com>
2023-02-27 16:22:21 +0100

    to-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value to 98
+ * @*n - pointer to the address of the value 
+ * Return: returns nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
