888eb880ef15e995d0cfbc2ad181a2888a21f22d
po-w-er <atanang.esthere@gmail.com>
2023-02-27 23:26:04 +0100

    push 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,18 +5,15 @@
  */
 void puts_half(char *str)
 {
-	int i, j;
+	int i = 0;
+    int j;
 
 	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
-		j = i / 2;
-	else
-		j = (i + 1) / 2;
-	while (j < i)
 	{
-		_putchar(str[j]);
-		j++;
+		i++;
 	}
-	_putchar('\n');
+  for(j = (i/2);j < i; j++){
+    _putchar(str[j]);
+}
+_putchar("\n");
 }
