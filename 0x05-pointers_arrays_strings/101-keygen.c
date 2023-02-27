0e64906dfedcef9d47c60810f4f55a5052d8ccc2
po-w-er <atanang.esthere@gmail.com>
2023-02-28 00:58:43 +0100

    file 7

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,8 +5,57 @@
  * main -
  * Return:
  */
-int main(void)
+void randomPassword(int N)
 {
-	srand(time(NULL));
-	return rand();
+    // Initialize counter
+    int i = 0;
+  
+    int randomizer = 0;
+  
+    
+    srand((unsigned int)(time(NULL)));
+  
+    // Array of numbers
+    char numbers[] = "0123456789";
+  
+    // Array of small alphabets
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+  
+    // Array of capital alphabets
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+  
+    
+    char symbols[] = "!@#$^&*?";
+  
+    
+    char password[N];
+  
+    
+    randomizer = rand() % 4;
+  
+    
+    for (i = 0; i < N; i++) {
+  
+        if (randomizer == 1) {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2) {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3) {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
+	return (0);
 }
