10ae6957ae47bfb8b94464500c86384406ac422e
po-w-er <atanang.esthere@gmail.com>
2023-02-28 00:35:30 +0100

    file 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,14 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-  for(j = (i/2);j < i; j++){
-    _putchar(str[j]);
-}
+	if(i % 2 == 0){
+	  for(j = (i/2); j < i; j++){
+	      _putchar(str[j]);
+	  }
+	}else{
+	    for(j = ((i/2)+1); j < i; j++){
+	       _putchar(str[j]);
+	  }
 _putchar('\n');
+}
 }
