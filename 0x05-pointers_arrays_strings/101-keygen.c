90532928cdfa0fdf23e5ab82b430f92c7d42037a
po-w-er <atanang.esthere@gmail.com>
2023-02-28 16:12:37 +0100

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,12 +7,12 @@
  */
 int main()
 {
-srand(time(NULL));
-int randomNumber;
 char *character = "wxy#+zMNO!PQRSHIB@=~>:Ao0189qrsptuv.CD23456$^&*()_-7EFGklmnJKLabcdefghijTUVWXYZ";
 char generatedPassword[15];
-  
-for(int i = 0; i < 15; i++){
+int randomNumber;
+int i;
+srand(time(NULL));
+for(i = 0; i < 15; i++){
 randomNumber = rand() % (79 + 1 - 0) + 0;
 generatedPassword[i] = character[randomNumber];
 }
