1b64db67f5712bb52a7de62eff6d2140b2e2e9c8
po-w-er <atanang.esthere@gmail.com>
2023-02-28 22:14:27 +0100

    pass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,15 +7,20 @@
  */
 int main()
 {
-char *character = "wxy#+zMNO!PQRSHIB@=~>:Ao0189qrsptuv.CD23456$^&*()_-7EFGklmnJKLabcdefghijTUVWXYZ";
-char generatedPassword[15];
-int randomNumber;
-int i;
+int pass[100];
+int i, sum, n;
+sum = 0;
 srand(time(NULL));
-for(i = 0; i < 15; i++){
-randomNumber = rand() % (79 + 1 - 0) + 0;
-generatedPassword[i] = character[randomNumber];
+for(i = 0; i < 100; i++){
+    pass[i] = rand() % 78;
+    sum += (pass[i] + '0');
+    putchar(pass[i] + '0');
+    if((2772 - sum)- '0' < 78){
+        n = 2772 - sum - '0';
+        sum += n;
+        putchar(n + '0');
+        break;
+    }
 }
-printf("%s", generatedPassword);
 return(0);
 }
