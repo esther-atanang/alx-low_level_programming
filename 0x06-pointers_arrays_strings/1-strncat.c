8a47460b04aca1c2c50abc022759564744914d7a
po-w-er <atanang.esthere@gmail.com>
2023-03-01 11:43:19 +0100

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+/**
+ *_strncat - a function that concatenates two strings
+ *@dest: the first string
+ *@src: the string that is going to be moved
+ *@n: the number of characters that would be moved
+ *Return: the character
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while(dest[i] != '\0')
+{
+i++;
+}
+while(j < n && j != '0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return dest;
+}
