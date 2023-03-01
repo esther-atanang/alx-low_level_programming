d0975566934debe343346a32b11b480a70529ebb
po-w-er <atanang.esthere@gmail.com>
2023-03-01 23:10:27 +0100

    corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
-while(dest[i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
-while(j < n && j != '0')
+while (j < n && j != '0')
 {
 dest[i] = src[j];
 i++;
 j++;
 }
-return dest;
+return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 /**
- * _strncpy - copies a string into another string
+ * _strncpy - copies a string into another
  * @dest: the destination
  * @src: the string that is going to be moved
  * @n: the size you want to be moved
@@ -8,10 +8,9 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for
-(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
-return dest;
+return (dest);
 }
