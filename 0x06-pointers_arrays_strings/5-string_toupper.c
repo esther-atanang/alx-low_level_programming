5a1972183f2af8ebd86be004fb649baa0da00af4
po-w-er <atanang.esthere@gmail.com>
2023-03-02 21:46:01 +0100

    new

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,14 +5,13 @@
  */
 char *string_toupper(char *c)
 {
-	int i;
-
-	i = 0;
-	while (c[i] != '\0')
-	{
-		if (c[i] >= 'a' && c[i] <= 'z')
-			c[i] -= 'a' - 'A';
-		i++;
-	}
-	return (c);
+int i;
+i = 0;
+while (c[i] != '\0')
+{
+if (c[i] >= 'a' && c[i] <= 'z')
+c[i] -= 'a' - 'A';
+i++;
+}
+return (c);
 }
