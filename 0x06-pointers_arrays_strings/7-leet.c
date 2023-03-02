a033f497039421ae2f18e8482df89bb798671385
po-w-er <atanang.esthere@gmail.com>
2023-03-02 12:05:37 +0100

    new

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,16 +5,15 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-	char subs[] = "aAeEoOtTlL";
-	char le[] = "43071";
-
-	i = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; subs[j] != '\0'; j++)
-			if (s[i] == subs[j])
-				s[i] = le[j / 2];
-	}
-	return (s);
+int i, j;
+char subs[] = "aAeEoOtTlL";
+char le[] = "43071";
+i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; subs[j] != '\0'; j++)
+if (s[i] == subs[j])
+s[i] = le[j / 2];
+}
+return (s);
 }
