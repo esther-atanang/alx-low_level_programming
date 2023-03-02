0684c070180c0e70175b03652bbd4667bfef509b
po-w-er <atanang.esthere@gmail.com>
2023-03-02 08:22:14 +0100

    corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	for (j = 0; dest[j] != '\0'; j++);
+	for (j = 0; dest[j] != '\0'; j++)
+    {
 	while (src[i] != '\0' && i < n)
 	{
 		dest[j + i] = src[i];
 		i++;
 	}
+    }
 	return (dest);
 }
