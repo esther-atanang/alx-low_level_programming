aeac6bcb57ce876a07b13191e117babfb5a4f098
po-w-er <atanang.esthere@gmail.com>
2023-03-02 23:28:36 +0100

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,10 @@
 void print_number(int n)
 {
 	int res, temp, expo;
-	
+
 	expo = 1;
 	if (n >= 0)
-	{
 		res = n * -1;
-	}
 	else
 	{
 		res = n;
@@ -23,12 +21,9 @@ void print_number(int n)
 	{
 		expo *= 10;
 		temp /= 10;
-		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
 	}
 	while (expo >= 1)
 	{
-
-		printf("expo: %d, res: %d\n", expo, ((res /expo) % 10) * -1);
 		_putchar(((res / expo) % 10) * -1 + '0');
 		expo /= 10;
 	}
