3b6b788089412a251aae294cca693cd2e2d40a75
po-w-er <atanang.esthere@gmail.com>
2023-03-07 02:36:51 +0100

    first

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,27 +4,14 @@
  * @a: pointer to 2D array.
  * @size: which diagonal to add.
  */
-void print_diagsums(int *a, int size){
-  int sum = 0; 
-  int sum2 = 0;
-  int f = size - 1;
-  int p = 0;
-  int i;
-  int j;
-    for(i = 0; i < size; i++){
-        for(j = 0; j < size; j++){
-            sum +=a[p];
-            break;
-        }
-        p = p + 1;
-    }
-    
-   for(i = 0; i < size; i++){
-        for(j = (size - 1); j > 0; j--){
-            sum2 += a[f];
-            printf("%d\n", sum2);
-            break;
-        }
-        f = f - 1;
-    } 
+void print_diagsums(int *a, int size)
+{
+	int i, sumx, sumy;
+
+	sumx = sumy = 0;
+	for (i = 0; i < (size * size); i += size + 1)
+		sumx += a[i];
+	for (i = size - 1; i < (size * size - 1); i += size - 1)
+		sumy += a[i];
+	printf("%d, %d\n", sumx, sumy);
 }
