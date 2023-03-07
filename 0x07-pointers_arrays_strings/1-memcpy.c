a8cc4472468dc7c49259f78ab30f7144e66735b9
po-w-er <atanang.esthere@gmail.com>
2023-03-07 02:16:32 +0100

    first

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 /**
- * _memcpy - a function that copies memory area.
- * @dest: the memory area
- * @src: the other memeory area 
- * @n: the number of bytes
- * Return: the memory space
-*/
+ * _memcpy - copies memory area
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: number of bytes to copy
+ * Return: pointer to destination memory area
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 { 
 unsigned int i = 0;
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,26 @@
-unsigned int _strspn(char *s, char *accept){
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to locate
+ * Return: pointer to first occurence of character c
+ * or NULL if character is not found.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
 int i = 0;
 int j;
 int k;
 unsigned int l = 0;
- while(*(s+i) != '\0'){
-    i++;
- }
- for(j= 0; j < i/2; j++){
-   for(k= 0; k < i/2; k++){
-      if(*(accept+j) == *(s+k)){
-         l++;
-      }
-   }
- }
-return l;
+while(*(s+i) != '\0')
+{
+i++;
+}
+for(j= 0; j < i/2; j++){
+for(k= 0; k < i/2; k++){
+if(*(accept+j) == *(s+k)){
+l++;
+}
+}
+}
+return(l);
 }
