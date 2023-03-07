af932abad10d9e65a8bd82a6bb42503eec35049f
po-w-er <atanang.esthere@gmail.com>
2023-03-07 02:48:37 +0100

    first

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,9 @@
-void set_string(char **s, char *to){
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to set
+ * @to: the char to point to.
+ */
+void set_string(char **s, char *to)
+{
     *s = to;
 }
