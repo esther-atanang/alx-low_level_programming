4d7b523f2af0bea5930df44a8530fb566f032b3a
po-w-er <atanang.esthere@gmail.com>
2023-03-07 02:23:34 +0100

    first

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,23 @@
-char *_strstr(char *haystack, char *needle){
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
+char *_strstr(char *haystack, char *needle)
+{
 int i = 0;
 int j;
-while(*(haystack+i) != '\0'){
-    i++;
- }
- for(j = 0; j < i; j++){
-     if(*(needle + 0) == *(haystack + j)){
-         return (haystack + j);
-         break;
-     }
- }
- return ('\0');
+while(*(haystack+i) != '\0')
+{
+i++;
+}
+for(j = 0; j < i; j++){
+if(*(needle + 0) == *(haystack + j))
+{
+return (haystack + j);
+break;
+}
+}
+return ('\0');
 }
