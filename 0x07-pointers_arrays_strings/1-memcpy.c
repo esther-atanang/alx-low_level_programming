800faaef6773f3c463b647fcc0557e43549444f8
po-w-er <atanang.esthere@gmail.com>
2023-03-07 02:02:42 +0100

    first

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,17 @@
-
-char *_memcpy(char *dest, char *src, unsigned int n){ 
-    unsigned int i = 0;
-    while(i < n){
-        *(dest + i) = src[i];
-       i++;
-    }
-     return dest;
+/**
+ * _memcpy - a function that copies memory area.
+ * @dest: pointer
+ * @src: constant byte
+ * @n: first bytes
+ * Return: a pointer to the memory space
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{ 
+unsigned int i = 0;
+while(i < n)
+{
+*(dest + i) = src[i];
+i++;
+}
+return (dest);
 }
