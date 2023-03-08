2cbdc64f7f8f6cfde5968e83a025b45c897d9712
po-w-er <atanang.esthere@gmail.com>
2023-03-08 23:24:10 +0100

    new file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 /**
- * _pow_recursion: returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: the number which is an integer
  * @y: the value it is raised by
  * Return: the value or -1 if y is below  0
 */
-int _pow_recursion(int x, int y)
+int _pow_recursion (int x, int y)
 {
 if (y < 0)
 {
