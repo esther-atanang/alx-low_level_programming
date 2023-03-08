06fe2e6cfa9123d200c0db65f45a7b78099dd4ff
po-w-er <atanang.esthere@gmail.com>
2023-03-08 21:58:23 +0100

    added print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _print_rev_recursion - it reverses a string
+ * @s: the string
+ * Return: nothing because it is void
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_puts_recursion((s + 1));
+_putchar(*s);
+}
+}
