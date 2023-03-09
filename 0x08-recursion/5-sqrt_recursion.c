f8c3a41265db5754f3ec0f310ce178065504a505
po-w-er <atanang.esthere@gmail.com>
2023-03-09 01:03:08 +0100

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,14 @@
-int _sqrt_recursion(int n){
+/**
+ * _sqrt_recursion - returns the squareroot of a number
+ * @n: the number which is an integer
+ * Return: the square root
+*/
+int _sqrt_recursion(int n)
+{
 int j = 0;
-if(j*j == n){
-    return j;
+if ((j * j) == n)
+{
+return (j);
 }
-return ((j+1) + _sqrt_recursion(n));
+return ((j + 1) + _sqrt_recursion(n));
 }
