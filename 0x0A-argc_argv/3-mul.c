e8cdc55320199f0a86bebfc10d917cd4200cdb6d
po-w-er <atanang.esthere@gmail.com>
2023-03-11 23:46:03 +0100

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,8 +8,8 @@
 */
 int main(int argc, char *argv[])
 {
-int multiply = 1;
 int i;
+int multiply = 1;
 if (argc == 3) 
 {
 for (i = 1; i < argc; i++)
@@ -18,7 +18,8 @@ multiply *= atoi(argv[i]);
 }
 printf("%d\n", multiply);
 }
-else{
+else
+{
 printf("Error\n");
 return (1);
 }
