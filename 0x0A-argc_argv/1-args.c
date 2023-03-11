0934cca4a0caaccfdec390878b92e92fddc77961
po-w-er <atanang.esthere@gmail.com>
2023-03-11 04:36:57 +0100

    new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,14 @@
  * @argv: simply stores the arguments
  * Return: 0 for success
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]){
+int i;
+if (argc && argv)
 {
-if (argc)
+for (i = 0; i < argc; i++)
 {
-printf("%s\n", argv[0]);
-}
+printf("%d\n", i);
+} 
+} 
 return (0);
 }
