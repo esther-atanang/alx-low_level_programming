2ac711012dd8194b1995776578bfe390cdd28540
po-w-er <atanang.esthere@gmail.com>
2023-03-11 23:44:05 +0100

    new

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - the primary function or start point
  * @argc: the number of arguments
@@ -10,35 +9,29 @@
 */
 int main(int argc, char *argv[])
 {
-    int i;
-    int sum = 0;
-
-    if (argc >= 2)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            char *c = argv[i];
-
-            while (*c)
-            {
-                if (!isdigit(*c))
-                {
-                    printf("Error\n");
-                    return 1;
-                }
-
-                c++;
-            }
-
-            sum += atoi(argv[i]);
-        }
-
-        printf("%d\n", sum);
-    }
-    else
-    {
-        printf("0\n");
-    }
-
-    return 0;
+int i;
+int sum = 0;
+if (argc >= 2)
+{
+for (i = 1; i < argc; i++)
+{
+char *c = argv[i];
+while (*c)
+{
+if (!isdigit(*c))
+{
+printf("Error\n");
+return (1);
+}
+c++;
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
 }
