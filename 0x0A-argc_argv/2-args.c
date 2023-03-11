ab8c8fbe2462419d6f3fb0ee52c6980ea4702523
po-w-er <atanang.esthere@gmail.com>
2023-03-11 16:45:50 +0100

    new

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - the primary function or start point
- *
  * @argc: the number of arguments
  * @argv: simply stores the arguments
- *
  * Return: 0 for success
 */
 int main(int argc, char *argv[])
 {
-    int i;
-    int num = 0;
-
-    if ((argc > 1) && argv)
-    {
-        for (i = 0; i < argc; i++)
-        {
-            num = i;
-        }
-        printf("%d\n", num);
-    }
-    else
-    {
-        printf("%d\n", num);
-    }
-
-    return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
