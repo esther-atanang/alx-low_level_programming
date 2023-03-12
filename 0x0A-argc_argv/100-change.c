7f88ee7b772ccc9282486c8637d3ae42b8327e06
po-w-er <atanang.esthere@gmail.com>
2023-03-12 01:21:31 +0100

    new

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - the primary function or start point
- * @argc: the number of arguments
- * @argv: simply stores the arguments
- * Return: 0 for success, 1 for failure
-*/
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 if successful, otherwise 1
+ */
 int main(int argc, char *argv[])
 {
-int c = 0;
-int number;
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-number = atoi(argv[1]);
-while (number > 0)
-{
-if (number >= 25)
-{
-number = number - 25;
-}
-else if (number < 25 && number >= 10)
-{
-number = number - 10;
-}
-else if (number < 10 && number >= 5)
-{
-number = number - 5;
-}
-else if (number < 5 && number >= 2)
-{
-number = number - 2;
-}
-else if (number < 2 && number == 1)
-{
-number = number - 1;  
-}
-c++;
-}
-printf("%d\n", c);
-return (0);
+    int number, c = 0;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    number = atoi(argv[1]);
+
+    while (number > 0)
+    {
+        if (number >= 25)
+        {
+            number -= 25;
+        }
+        else if (number >= 10)
+        {
+            number -= 10;
+        }
+        else if (number >= 5)
+        {
+            number -= 5;
+        }
+        else if (number >= 2)
+        {
+            number -= 2;
+        }
+        else if (number == 1)
+        {
+            number -= 1;
+        }
+
+        c++;
+    }
+
+    printf("%d\n", c);
+
+    return (0);
 }
