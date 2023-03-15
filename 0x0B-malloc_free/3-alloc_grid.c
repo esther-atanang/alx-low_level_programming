804e0dc0cc7681028ca5309f29c5e2bbd3535f42
Po-w-er <atanang.esthere@gmail.com>
2023-03-16 00:33:23 +0100

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,14 +9,15 @@ int **alloc_grid(int width, int height)
 {
 int **arr;
 int i;
+int k;
 int j;
 if ((width <= 0)||(height <= 0))
 {
 return (NULL);
 }
 arr = malloc(width * sizeof(int *));
-for (int i = 0; i < width; i++) {
-    arr[i] = malloc(height * sizeof(int));
+for (k = 0; i < width; k++) {
+    arr[k] = malloc(height * sizeof(int));
 }
 if (arr == NULL)
 {
