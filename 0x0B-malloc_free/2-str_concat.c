6142755edfcef92280c81bbd193b7ae0fa4b9119
Po-w-er <atanang.esthere@gmail.com>
2023-03-15 23:24:35 +0100

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,27 +7,42 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-int len1 = 0, len2 = 0, i = 0, j = 0;
+int len1 = 0, len2 = 0, i = 0,j = 0;
 char *newStr;
 if (s1 != NULL)
-while (s1[len1] != '\0')
-len1++;
-if (s2 != NULL)
-while (s2[len2] != '\0')
-len2++;
+    while (s1[len1] != '\0')
+    {
+    len1++;
+    }
+if(s2 != NULL)
+    while (s2[len2] != '\0')
+    {
+    len2++;
+    }
 newStr = malloc((len1 + len2 + 1) * sizeof(char));
 if (newStr == NULL)
-return (NULL);
+    return (NULL);
 while (i <= (len1 + len2))
-if ((len1 + len2) == 1)
-newStr[len1 + len2] = '\0';
-if (i < len1 && len1 > 0)
-if (s1 != NULL)
-newStr[i] = s1[i];
-if (i >= len1 && len2 > 0)
-if (s2 != NULL)
-newStr[i] = s2[j], j++;
-newStr[len1 + len2] = '\0';
-i++;
+    if ((len1 + len2) == 1)
+    {
+    newStr[len1 + len2] = '\0';
+    }
+    if (i < len1 && len1 > 0)
+    {
+    if(s1 != NULL)
+    {
+    newStr[i] = s1[i];
+    }
+    }
+    if (i >= len1 && len2 > 0)
+    {
+    if (s2 != NULL)
+    {
+    newStr[i] = s2[j];
+    j++;
+    }
+    }
+    newStr[len1 + len2] = '\0';
+    i++;
 return (newStr);
 }
