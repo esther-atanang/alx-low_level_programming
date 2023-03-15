5543b8e8e7f521605a22f3c7047ad50e69f69bd4
Po-w-er <atanang.esthere@gmail.com>
2023-03-15 23:15:55 +0100

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,52 +7,27 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-int len1 = 0;
-int len2 = 0;
-int i = 0;
-int j = 0;
+int len1 = 0, len2 = 0, i = 0, j = 0;
 char *newStr;
 if (s1 != NULL)
-{
 while (s1[len1] != '\0')
-{
 len1++;
-}
-}
-if(s2 != NULL)
-{
+if (s2 != NULL)
 while (s2[len2] != '\0')
-{
 len2++;
-}
-}
 newStr = malloc((len1 + len2 + 1) * sizeof(char));
 if (newStr == NULL)
-{
 return (NULL);
-}
 while (i <= (len1 + len2))
-{
-if((len1 + len2) == 1){
+if ((len1 + len2) == 1)
 newStr[len1 + len2] = '\0';
-}
 if (i < len1 && len1 > 0)
-{
-if(s1 != NULL)
-{
+if (s1 != NULL)
 newStr[i] = s1[i];
-}
-}
 if (i >= len1 && len2 > 0)
-{
 if (s2 != NULL)
-{
-newStr[i] = s2[j];
-j++;
-}
-}
+newStr[i] = s2[j], j++;
 newStr[len1 + len2] = '\0';
 i++;
-}
 return (newStr);
 }
