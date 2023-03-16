a46146c8dbb77b83d640d18954d9d87a427ccba6
Po-w-er <atanang.esthere@gmail.com>
2023-03-17 00:43:53 +0100

    first

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,11 @@
 #include <stdlib.h>
+/**
+ * string_nconcat - returns a pointer
+ * @s1: first string
+ * @s2: second string
+ * @n: the number of bytes
+ * Return: the pointer
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i = 0;
@@ -6,9 +13,9 @@ int p = 0;
 unsigned int k = 0;
 unsigned int size;
 char *newstr;
-if(s1 != NULL)
+if (s1 != NULL)
 {
-while( s1[i] != '\0')
+while (s1[i] != '\0')
 {
 i++;
 }
@@ -17,22 +24,22 @@ i++;
 newstr = malloc((i + n + 1)*sizeof(char));
 if (newstr == NULL)
 {
-return NULL;
+return (NULL);
 }
 size = i + n;
-while(k <= size)
+while (k <= size)
 {
- if( k < n && s1 != NULL) 
-  {
-  newstr[k] = s1[k];
-  }
-  if(k >= i && s2 != NULL)
-  {
-    newstr[k] = s2[p];
-    p++;
-  }
-  newstr[size] = '\0';
-  k++;
+if ( k < n && s1 != NULL) 
+{
+newstr[k] = s1[k];
 }
-return newstr;
+if (k >= i && s2 != NULL)
+{
+newstr[k] = s2[p];
+p++;
+}
+newstr[size] = '\0';
+k++;
+}
+return (newstr);
 }
