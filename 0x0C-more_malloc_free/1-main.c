c7d5c91dd898ac9d9c7a8c5c05ace328f5d445ef
Po-w-er <atanang.esthere@gmail.com>
2023-03-17 01:04:02 +0100

    new

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("Bestyruhkwb", "Scho !!!", 6);
+    concat = string_nconcat("Bestyruhkwb", NULL, 6);
     printf("%s\n", concat);
     free(concat);
     return (0);
