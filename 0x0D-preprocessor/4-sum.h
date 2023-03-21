37207dd402345705db6bc736ef588082b5d3f602
Po-w-er <atanang.esthere@gmail.com>
2023-03-21 19:47:10 +0100

    new

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,4 @@
-/**
- * SUM - finds the sum of a number
- * @x: the first number
- * @y: the second number
- * Return: the sum of the number
-*/
+#ifndef SUM
+#define SUM
 #define SUM(x, y) ((x) + (y))
+#endif
