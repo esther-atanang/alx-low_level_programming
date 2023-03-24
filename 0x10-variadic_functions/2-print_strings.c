becd775073bc899e4344b951e198e6df31ed7ee2
Po-w-er <atanang.esthere@gmail.com>
2023-03-24 14:08:16 +0100

    updated the print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,18 +9,23 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int counter;
+char *word;
 va_list(args);
 va_start(args, n);
 for (counter = 0; counter < n; counter++)
 {
-printf("%s", (char *)va_arg(args, char *));
+word = (char *)va_arg(args, char *);
+if(word == NULL)
+{
+printf("nil");
+}
+else
+{
+printf("%s", word);
+}
 if (separator && counter != (n-1))
 {
 printf("%s", separator);
-}
-else if(!(separator) && counter != (n-1))
-{
-printf("nil");
 }
 }
 printf("\n");
