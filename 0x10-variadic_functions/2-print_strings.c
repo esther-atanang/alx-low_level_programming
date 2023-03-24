0282436e7107e38114edc3aa96fcadf504db0c9e
Po-w-er <atanang.esthere@gmail.com>
2023-03-24 13:56:13 +0100

    updated files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 /**
  * print_string - prints strings
- * 
+ * @separator: character printed in between the strings
+ * @n: the number of argumnets
+ * Return: Nothing
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int counter;
 va_list(args);
 va_start(args, n);
-for(counter = 0; counter < n; counter++)
+for (counter = 0; counter < n; counter++)
 {
 printf("%s", (char *)va_arg(args, char *));
-if(separator && counter != (n-1))
+if (separator && counter != (n-1))
 {
 printf("%s", separator);
+}
+else
+{
+printf("nil");
 }
 }
 printf("\n");
