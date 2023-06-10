b5e02eefc40b608f77c57274453a3845cf86d6a7
Po-w-er <atanang.esthere@gmail.com>
2023-06-10 10:48:29 +0100

    new changes

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-<<<<<<< HEAD
 /**
  * _putchar - returns a character
  * @c: the charater it returns
@@ -8,11 +7,9 @@
 void _putchar(char c)
 {
 write(1,&c,1);
-=======
 
 int _putchar(char c)
 {
     write(1, &c, 1);
     return (0);
->>>>>>> ef42ba6 (updated the library file)
 }
