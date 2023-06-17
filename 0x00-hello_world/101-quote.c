81f1cd67200d5789b1a96fce97fe3866a5e9c04e
Po-w-er <atanang.esthere@gmail.com>
2023-06-17 19:23:02 +0100

    update file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-    fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\"\n");
+    fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
     return(1);
 }
