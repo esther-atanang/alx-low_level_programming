b8c6e36e36455f894d050c67c4e8bc49faf70b31
Po-w-er <atanang.esthere@gmail.com>
2023-06-17 19:16:00 +0100

    updated file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  * Return: 0 on success
  */
-void main(void)
+int main(void)
 {
     fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\"\n");
     return(1);
