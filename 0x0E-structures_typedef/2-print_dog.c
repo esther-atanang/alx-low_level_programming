00a8414bfdbbb639fc0f32c45db5ae3c40277968
Po-w-er <atanang.esthere@gmail.com>
2023-06-20 01:08:39 +0100

    new filw

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
  * print_dog - Prints the information of a dog
  * @d: Pointer to the dog structure
+ *
+ * Description: This function prints the name, age, and owner of the dog.
+ *              If any of the fields are NULL, it prints "(nil)" instead.
+ *              If the age is not positive, it prints "(nil)" for the age field.
+ *
  * Return: Nothing
  */
 void print_dog(struct dog *d)
 {
     if (d != NULL)
     {
-        if (d->age > 0.0)
+        if (d->age > 0.000001) /* Compare with a small positive value */
         {
-            printf("%s\n%.6f\n%s\n", (d->name != NULL) ? d->name : "(nil)", d->age, 
-            (d->owner != NULL) ? d->owner : "(nil)");
+            printf("Name: %s\nAge: %.6f\nOwner: %s\n",
+                   (d->name != NULL) ? d->name : "(nil)",
+                   (d->age),
+                   (d->owner != NULL) ? d->owner : "(nil)");
         }
         else
         {
-            printf("%s\n%s\n%s\n", (d->name != NULL) ? d->name : "(nil)", 
-            "(nil)", (d->owner != NULL) ? d->owner : "(nil)");
+            printf("Name: %s\nAge: %s\nOwner: %s\n",
+                   (d->name != NULL) ? d->name : "(nil)",
+                   "(nil)",
+                   (d->owner != NULL) ? d->owner : "(nil)");
         }
     }
 }
