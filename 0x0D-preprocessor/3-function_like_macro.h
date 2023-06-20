776e7084a6413180d07f2f7223246719ce9ba03a
Po-w-er <atanang.esthere@gmail.com>
2023-06-20 22:39:40 +0100

    new

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-/**
- * ABS_H: finds the absolute of a number
-*/
-#ifndef ABS_H
-#define ABS_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
 #define ABS(x) (x < 0 ? x * -1: x)
+
 #endif
