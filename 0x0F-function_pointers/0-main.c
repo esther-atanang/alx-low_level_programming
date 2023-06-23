a786a24689bc5e3596a82d62f7aa5e43619efe61
Po-w-er <atanang.esthere@gmail.com>
2023-06-23 08:22:52 +0100

    updated file

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -45,7 +46,7 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
+    print_name("Bob", NULL);
     print_name("Bob Dylan", print_name_uppercase);
     printf("\n");
     return (0);
--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - Prints a name
  * @name: Pointer to a string containing the name
@@ -8,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if(f != NULL){
 f(name);
+}
 }
