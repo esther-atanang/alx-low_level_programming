dacd8effe57f0c85098316e6bdf118e300e3d13f
Po-w-er <atanang.esthere@gmail.com>
2023-06-23 08:39:54 +0100

    updated the array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,9 +4,8 @@
  * @array: Pointer to the array
  * @size: The size of the array
  * @action: Pointer to the function that performs the action
- *
  * Description: This function iterates over the elements of the array and
- *              applies the provided action function to each element.
+ * applies the provided action function to each element.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
